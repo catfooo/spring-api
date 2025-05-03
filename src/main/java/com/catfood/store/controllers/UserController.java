21c8ff98117ebabff9a30dc50d46a732bdf2ca63
So Youn Choi <manghatuhyo@gmail.com>
2025-05-03 11:36:38 +0200

    sort

--- src/main/java/com/catfood/store/controllers/UserController.java
+++ src/main/java/com/catfood/store/controllers/UserController.java
@@ -1,18 +1,14 @@
 package com.catfood.store.controllers;
 
 import com.catfood.store.dtos.UserDto;
-import com.catfood.store.entities.User;
 import com.catfood.store.mappers.UserMapper;
 import com.catfood.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -22,9 +18,13 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    // method: GET
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(
+            @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy
+    ) {
+        if (!Set.of("name", "email").contains(sortBy))
+            sortBy = "name";
+
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
