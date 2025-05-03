10765dcf45d935f00f3dbd99c22bfe066302e4bc
So Youn Choi <manghatuhyo@gmail.com>
2025-05-03 11:10:35 +0200

    to edit json

--- src/main/java/com/catfood/store/mappers/UserMapper.java
+++ src/main/java/com/catfood/store/mappers/UserMapper.java
@@ -3,8 +3,10 @@ package com.catfood.store.mappers;
 import com.catfood.store.dtos.UserDto;
 import com.catfood.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+//    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
